1f2f3e015d5357a0fd661aff2492de947d00b09d
joaopedro877 <jdebritocoutinhoferreira@gmail.com>
2022-11-10 17:06:02 -0300

    .

--- index.js
+++ index.js
@@ -4,15 +4,34 @@ let sum = firstCard+secondCard
 let hasBlackJack=false
 let isAlive=true
 let message=''
+let messageEl=document.getElementById("message-el")
+console.log(messageEl)
+let sumEl=document.getElementById("sum-el")
+let cardsEl=document.getElementById("cards-el")
 
-if(sum <= 20){
-    message="Do you want to draw a new card?"
-}else if (sum===21){
-    message="You've got BlackJack!"
-    hasBlackJack=true
-} else{
-     message="You're out of the game!"
-    isAlive=false
+function renderGame(){
+    sumEl.textContent="Sum: "+sum
+    cardsEl.textContent="Cards:" +" "+firstCard+" "+secondCard
+    if(sum <= 20){
+        message="Do you want to draw a new card?"
+    }else if (sum===21){
+        message="You've got BlackJack!"
+        hasBlackJack=true
+    } else{
+         message="You're out of the game!"
+        isAlive=false
+    }
+    console.log(message)
+    messageEl.textContent=message
 }
-console.log(message)
 
+function newCard(){
+    let card=4
+    sum+=card
+    renderGame()
+    cardsEl.textContent+=" "+card
+}
+function startGame(){
+    renderGame()
+}
+//2:30h
